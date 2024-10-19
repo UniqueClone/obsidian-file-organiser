abe9955245ca7f86bb74bc26812595b3f884a21b
Ryan Lynch <ryanlynch@microsoft.com>
2024-10-19 18:21:25 +0100

    Creating new plugin

--- main.ts
+++ main.ts
@@ -1,134 +1,121 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Notice, Plugin, TFolder } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class FolderOrganisationPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		// await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.moveFilesToFolder();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "move-files-to-folder",
+			name: "Move Files to Folder",
 			callback: () => {
-				new SampleModal(this.app).open();
+				this.moveFilesToFolder();
+			},
+		});
+	}
+
+	onunload() {}
+
+	// async loadSettings() {
+	// 	this.settings = Object.assign(
+	// 		{},
+	// 		DEFAULT_SETTINGS,
+	// 		await this.loadData()
+	// 	);
+	// }
+
+	// async saveSettings() {
+	// 	await this.saveData(this.settings);
+	// }
+
+	async moveFilesToFolder() {
+		// Get all files in the vault
+		const files = await this.app.vault.getMarkdownFiles();
+		//
+
+		// Get all folders in the vault
+		const folders: TFolder[] = []; // = this.app.vault.getRoot();
+		// files.map((file) => file.parent).unique();
+
+		for (const file of files) {
+			const parent = file.parent;
+			if (parent && !folders.includes(parent) && !parent.isRoot()) {
+				folders.push(parent);
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+		}
+
+		for (const file of files) {
+			// Find the folder that the file should be in
+			const folder = folders.find((folder) =>
+				file.basename
+					.toLowerCase()
+					.contains(folder?.name.toLowerCase() ?? "")
+			);
+
+			let shouldMove = false;
+
+			// Make sure there is no space before or after the matching folder name in the file name
+			if (folder) {
+				const folderName = folder.name.toLowerCase();
+				const fileName = file.basename.toLowerCase();
+
+				// Check if the folder name is in the file name
+				const folderNameIndex = fileName.indexOf(folderName);
+				const folderNameLength = folderName.length;
+				const folderNameEndIndex = folderNameIndex + folderNameLength;
+
+				// Check if there is a space before the folder name
+				const hasSpaceBefore =
+					folderNameIndex > 0 &&
+					fileName[folderNameIndex - 1] === " ";
+
+				// Check if there is a space after the folder name
+				const hasSpaceAfter =
+					folderNameEndIndex < fileName.length &&
+					fileName[folderNameEndIndex] === " ";
+
+				// Check if the folder name is at the beginning of the file name
+				const isAtBeginning = folderNameIndex === 0;
+
+				// Check if the folder name is at the end of the file name
+				const isAtEnd = folderNameEndIndex === fileName.length;
+
+				// Check if the folder name is the entire file name
+				const isEntireName =
+					folderNameIndex === 0 &&
+					folderNameEndIndex === fileName.length;
+
+				shouldMove =
+					isEntireName ||
+					(isAtBeginning && hasSpaceAfter) ||
+					(isAtEnd && hasSpaceBefore) ||
+					(hasSpaceBefore && hasSpaceAfter);
 			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			// If the file is not in the correct folder, move it
+			// Also, don't move the file if it's in the .obsidian folder
+			// Also, don't move the file if the folder is the root folder
+			if (
+				folder &&
+				file?.parent?.path !== folder.path &&
+				// !file.path.contains(".obsidian") &&
+				!folder.isRoot() &&
+				shouldMove
+			) {
+				// Move the file to the correct folder
+				await this.app.fileManager
+					.renameFile(
+						this.app.vault.getFileByPath(file.path) ?? file,
+						folder.path + "/" + file.name
+					)
+					.catch((error) => {
+						new Notice(`Error moving file...${file.basename}`);
+						new Notice(error);
+					});
+
+				new Notice(`Moved ${file.basename} to ${folder.name} folder.`);
 			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		}
 	}
 }
